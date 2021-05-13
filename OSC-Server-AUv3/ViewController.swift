0755e4f9ed818b786521236840ee32660cfa0c50
Frans-Jan Wind <fjw@yourule.nl>
2021-05-13 14:04:39 +0200

    OSC framework added

--- OSC-Server-AUv3/ViewController.swift
+++ OSC-Server-AUv3/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import SwiftOSC
+
+var serverOSC = OSCServer(address: "", port: 3545)
+
+
 
 class ViewController: UIViewController {
-
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        serverOSC.start()
+        
+        class OSCHandler: OSCServerDelegate {
+            
+            func didReceive(_ message: OSCMessage){
+                if let integer = message.arguments[0] as? Int {
+                    print("Received int \(integer)")
+                } else {
+                    print(message)
+                }
+            }
+        }
+        serverOSC.delegate =  OSCHandler()
+    
     }
-
-
 }
 
